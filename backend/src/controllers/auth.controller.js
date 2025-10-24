014848bdeaebe1f5c2424d20bd8e60cf70bab02f
Alexandru Rădulescu <alex11amr110506@gmail.com>
2025-10-24 21:57:50 +0300

    more bugs

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -120,7 +120,7 @@ export async function editProfile(req,res) {
     try {
         const {fullName,email,location,university,linkGit}=req.body
         const userId=req.user._id
-        const user=User.findById(userId)
+        const user=await User.findById(userId)
 
         if(!user){
             return res.status(404).json({ message: "User not found." });
